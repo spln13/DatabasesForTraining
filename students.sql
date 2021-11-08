d5f707bcfe8ff58239aa2d57845d8fa49d2d8236
spln <805133358@qq.com>
2021-11-08 21:56:29 +0800

    add some data

--- students.sql
+++ students.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80026
  File Encoding         : 65001
 
- Date: 08/11/2021 21:32:00
+ Date: 08/11/2021 21:56:10
 */
 
 SET NAMES utf8mb4;
@@ -45,6 +45,7 @@ INSERT INTO `c` VALUES ('6', '数据处理', '5', 2);
 INSERT INTO `c` VALUES ('7', 'PASCAL语言', '6', 4);
 INSERT INTO `c` VALUES ('8', '离散数学', NULL, 3);
 INSERT INTO `c` VALUES ('9', '线性代数', '2', 3);
+INSERT INTO `c` VALUES ('C2', '计算机图形学', '7', 2);
 COMMIT;
 
 -- ----------------------------
@@ -72,6 +73,9 @@ INSERT INTO `s` VALUES ('201215124', '王志', 20, '男', '信科');
 INSERT INTO `s` VALUES ('201215125', '张立', 19, '男', '信科');
 INSERT INTO `s` VALUES ('201215126', '万芳', 19, '女', '文学');
 INSERT INTO `s` VALUES ('201215127', '张三', 24, '男', '财管');
+INSERT INTO `s` VALUES ('201215128', '韩金龙', 18, '男', '数据');
+INSERT INTO `s` VALUES ('201215129', '佟丽娅', 18, '女', '文学');
+INSERT INTO `s` VALUES ('201215130', '柳君', 18, '女', '化工');
 INSERT INTO `s` VALUES ('95010101', '赵清', 25, '男', '计算机');
 INSERT INTO `s` VALUES ('95010102', '王一', 21, '女', '计算机');
 INSERT INTO `s` VALUES ('95010103', '李美', 21, '男', '计算机');
@@ -79,7 +83,7 @@ INSERT INTO `s` VALUES ('95010201', '人门', 24, '女', '计算机');
 INSERT INTO `s` VALUES ('95010202', '王晓', 23, '女', '计算机');
 INSERT INTO `s` VALUES ('95010203', '李鸿', 25, '女', '计算机');
 INSERT INTO `s` VALUES ('95010204', '打法', 24, '男', '计算机');
-INSERT INTO `s` VALUES ('95010205', '距离', 24, '女', '计算机');
+INSERT INTO `s` VALUES ('95010205', '居丽', 24, '女', '计算机');
 INSERT INTO `s` VALUES ('95020101', '张会', 22, '男', '化工');
 INSERT INTO `s` VALUES ('95020102', '好人', 21, '男', '化工');
 INSERT INTO `s` VALUES ('95020103', '会类', 24, '男', '化工');
@@ -114,12 +118,14 @@ INSERT INTO `sc` VALUES ('201215121', '4', 87);
 INSERT INTO `sc` VALUES ('201215121', '5', 91);
 INSERT INTO `sc` VALUES ('201215121', '7', 88);
 INSERT INTO `sc` VALUES ('201215121', '9', 82);
+INSERT INTO `sc` VALUES ('201215121', 'C2', 99);
 INSERT INTO `sc` VALUES ('201215122', '10', 97);
 INSERT INTO `sc` VALUES ('201215122', '2', 90);
 INSERT INTO `sc` VALUES ('201215122', '3', 80);
 INSERT INTO `sc` VALUES ('201215127', '1', 86);
 INSERT INTO `sc` VALUES ('201215127', '10', 70);
 INSERT INTO `sc` VALUES ('201215127', '4', 78);
+INSERT INTO `sc` VALUES ('201215127', 'C2', 59);
 INSERT INTO `sc` VALUES ('95010101', '1', 80);
 INSERT INTO `sc` VALUES ('95010101', '2', 100);
 INSERT INTO `sc` VALUES ('95010101', '3', 80);
@@ -130,6 +136,7 @@ INSERT INTO `sc` VALUES ('95010102', '2', 80);
 INSERT INTO `sc` VALUES ('95010102', '3', 80);
 INSERT INTO `sc` VALUES ('95010102', '4', 56);
 INSERT INTO `sc` VALUES ('95010102', '5', 80);
+INSERT INTO `sc` VALUES ('95010102', 'C2', 88);
 INSERT INTO `sc` VALUES ('95010103', '1', 80);
 INSERT INTO `sc` VALUES ('95010103', '10', 99);
 INSERT INTO `sc` VALUES ('95010103', '2', 80);
@@ -162,6 +169,7 @@ INSERT INTO `sc` VALUES ('95010205', '3', 80);
 INSERT INTO `sc` VALUES ('95010205', '4', 80);
 INSERT INTO `sc` VALUES ('95010205', '5', 80);
 INSERT INTO `sc` VALUES ('95010205', '6', 80);
+INSERT INTO `sc` VALUES ('95010205', 'C2', 78);
 INSERT INTO `sc` VALUES ('95020101', '1', 80);
 INSERT INTO `sc` VALUES ('95020101', '10', 99);
 INSERT INTO `sc` VALUES ('95020101', '2', 80);
