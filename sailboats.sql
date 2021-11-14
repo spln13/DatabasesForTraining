a456cb9ee009872e38547ab0011e3de8886db36e
spln <805133358@qq.com>
2021-11-14 15:11:10 +0800

    add function of create database

--- sailboats.sql
+++ sailboats.sql
@@ -1,76 +1,96 @@
--- insert data sailboats for MySQL
--- Author wyh
--- 说明：打开此.sql文件，执行一次即可将数据插入已经建好的 sailboats 数据库中
-use sailboats;
-set names utf8;
+/*
+ Navicat Premium Data Transfer
 
-DROP TABLE IF EXISTS `sailors`;
-CREATE TABLE `sailors` (
-    `Snum` varchar(20) DEFAULT NULL, 
-    `Sname` varchar(20) DEFAULT NULL,
-    `val` int DEFAULT NULL,
-    `Sage` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80026
+ Source Host           : localhost:3306
+ Source Schema         : sailboats
 
+ Target Server Type    : MySQL
+ Target Server Version : 80026
+ File Encoding         : 65001
+
+ Date: 14/11/2021 15:10:27
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for boats
+-- ----------------------------
 DROP TABLE IF EXISTS `boats`;
 CREATE TABLE `boats` (
-    `num` varchar(20) DEFAULT NULL, 
-    `name` varchar(20) DEFAULT NULL,
-    `color` varchar(20) DEFAULT NULL
+  `bid` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `bname` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `color` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`bid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
+-- ----------------------------
+-- Records of boats
+-- ----------------------------
+BEGIN;
+INSERT INTO `boats` VALUES ('101', 'Interlake', 'Blue');
+INSERT INTO `boats` VALUES ('102', 'Interlake', 'Red');
+INSERT INTO `boats` VALUES ('103', 'Clipper', 'Green');
+INSERT INTO `boats` VALUES ('104', 'Marine', 'Red');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for reserves
+-- ----------------------------
 DROP TABLE IF EXISTS `reserves`;
 CREATE TABLE `reserves` (
-    `Snum` varchar(20) DEFAULT NULL, 
-    `Bnum` varchar(20) DEFAULT NULL,
-    `date` date DEFAULT NULL
+  `sid` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `big` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `day` date NOT NULL,
+  PRIMARY KEY (`sid`,`big`,`day`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-insert into sailors
-values('22','Dustin',7,45);
-insert into sailors
-values('29','Brutus',1,33);
-insert into sailors
-values('31','Lubber',8,55);
-insert into sailors
-values('32','Andy',8,25);
-insert into sailors
-values('58','Rusty',10,35);
-insert into sailors
-values('64','Horatio',7,35);
-insert into sailors
-values('71','Zorba',10,16);
-insert into sailors
-values('74','Jeff',9,35);
-insert into sailors
-values('85','Art',3,25);
-insert into sailors
-values('95','Bob',3,63);
-insert into boats
-values('101','Interlake','Blue');
-insert into boats
-values('102','Interlake','Red');
-insert into boats
-values('103','Clipper','Green');
-insert into boats
-values('104','Marine','Red');
-insert into reserves
-values('22','101','1998-10-10');
-insert into reserves
-values('22','102','1998-10-10');
-insert into reserves
-values('22','103','1998-10-8');
-insert into reserves
-values('22','104','1998-10-7');
-insert into reserves
-values('31','102','1998-11-10');
-insert into reserves
-values('31','103','1998-11-6');
-insert into reserves
-values('31','104','1998-11-12');
-insert into reserves
-values('64','101','1998-9-5');
-insert into reserves
-values('64','102','1998-9-5');
-insert into reserves
-values('74','103','1998-9-8');
+-- ----------------------------
+-- Records of reserves
+-- ----------------------------
+BEGIN;
+INSERT INTO `reserves` VALUES ('22', '101', '1998-10-10');
+INSERT INTO `reserves` VALUES ('22', '102', '1998-10-10');
+INSERT INTO `reserves` VALUES ('22', '103', '1998-10-08');
+INSERT INTO `reserves` VALUES ('22', '104', '1998-10-07');
+INSERT INTO `reserves` VALUES ('31', '102', '1998-11-10');
+INSERT INTO `reserves` VALUES ('31', '103', '1998-11-06');
+INSERT INTO `reserves` VALUES ('31', '104', '1998-11-12');
+INSERT INTO `reserves` VALUES ('64', '101', '1998-09-05');
+INSERT INTO `reserves` VALUES ('64', '102', '1998-09-05');
+INSERT INTO `reserves` VALUES ('74', '103', '1998-09-08');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for sailors
+-- ----------------------------
+DROP TABLE IF EXISTS `sailors`;
+CREATE TABLE `sailors` (
+  `sid` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sname` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- ----------------------------
+-- Records of sailors
+-- ----------------------------
+BEGIN;
+INSERT INTO `sailors` VALUES ('22', 'Dustin', 7, 45);
+INSERT INTO `sailors` VALUES ('29', 'Brutus', 1, 33);
+INSERT INTO `sailors` VALUES ('31', 'Lubber', 8, 55);
+INSERT INTO `sailors` VALUES ('32', 'Andy', 8, 25);
+INSERT INTO `sailors` VALUES ('58', 'Rusty', 10, 35);
+INSERT INTO `sailors` VALUES ('64', 'Horatio', 7, 35);
+INSERT INTO `sailors` VALUES ('71', 'Zorba', 10, 16);
+INSERT INTO `sailors` VALUES ('74', 'Jeff', 9, 35);
+INSERT INTO `sailors` VALUES ('85', 'Art', 3, 25);
+INSERT INTO `sailors` VALUES ('95', 'Bob', 3, 63);
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
